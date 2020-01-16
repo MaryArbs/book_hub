84aa09cfa388dbdba255cd84f2f6747358f9c5a2
MaryArbs <mary.arbachauskas@gmail.com>
2020-01-15 20:07:49 -0500

    update views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,22 @@ require './config/environment'
 class UsersController < Sinatra::Base
 
 
-  # get "/signup" do
-  #   erb :'user/signup'
-  # end
+  get "/signup" do
+    erb :'user/signup'
+  end
 
   post "/signup" do
     if params[:username].empty?
-      redirect to '/failure'
+      redirect to '/signup'
     end
+    if params[:password].emtpy
+      redirect to '/signup'
+    end 
       user = User.new(:username => params[:username], :password => params[:password])
     if user.save
       redirect '/login'
     else
-      redirect '/failure'
+      redirect '/signup'
     end
   end
   
