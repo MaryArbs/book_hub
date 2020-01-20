e9dc09b53ebdab158d8814bcb33af9d4fb6fb8bd
MaryArbs <mary.arbachauskas@gmail.com>
2020-01-19 23:42:06 -0500

    css styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,24 +11,22 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    binding.pry
+  
     erb :homepage
   end 
 
  
   helpers do
 
-
     def logged_in?
-      !!session[:user_id]
+        !!current_user
     end
 
-    def current_user
-      User.find(session[:user_id])
-      
+    def current_user # if a is undefined or falsey, then evaluate b and set a to the result. Equivalently, if a is defined and evaluates to truthy, then b is not evaluated, and no assignment takes place.
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+       end
     end
   end 
 
-end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     else
       redirect "/signup"
     end
+  end 
     
     
   get '/signup' do
